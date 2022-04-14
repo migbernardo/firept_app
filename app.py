4ccedc57ecba83fc34ece3ffd39f20e2e5090e8c
migbernardo <mbdexb@gmail.com>
2022-04-14 23:30:11 +0100

    update

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([
-            html.H1('Fires in Portugal: An In-Depth Look')
+            html.H1('Fires in Portugal: A Closer Look')
         ], className='column', style={'width': '50%'}),
         html.Div([
             html.Img(src=app.get_asset_url("Nova_IMS.png"),
                      style={"height": "60px", "width": "auto", "margin-bottom": "25px"}, className='image')
         ], className='column', style={'width': '50%'}),
 
-    ], className='title'),
+    ], className='title', style={'padding': '1%'}),
 
-    html.Div([html.H6('Environmental, human, and economic losses due to fires are an ever-present and increasing '
+    html.Div([html.P('Environmental, human, and economic losses due to fires are an ever-present and increasing '
                       'threat in Portugal. ’Megafires’ similar to the 2017 Pedrógão Grande fire have been identified '
                       'by the EU Climate Action and Resource Efficiency Unit as a major concern, since they challenge '
                       'the suppression capacities of many wildfire protection programs. The unit indicates that '
@@ -41,7 +41,7 @@ app.layout = html.Div([
                       'managing fires, and should be integrated into fire-related policies at local, national, '
                       'and EU levels, empowered by local communities. This dashboard is presented to aid in the EU '
                       'call to action, and to reinforce the exchange of information and collaboration in regards to '
-                      'fires within the Portuguese community.')]),
+                      'fires within the Portuguese community.')], style={'padding': '1%', 'font-size': '120%'}),
 
     html.Div([dcc.Slider(2001, 2018, 1, value=2001, marks={i: '{}'.format(i) for i in range(2001, 2019)},
                          tooltip={"placement": "bottom", "always_visible": True},
@@ -53,7 +53,15 @@ app.layout = html.Div([
         html.Div([
 
             html.Div([
-                html.H6('Title')
+                html.P('In the visualization below on the left you can see which regions had the most burnt area in '
+                       'square hectares per year. For the visualization on the right you can see which counties had '
+                       'the most burnt area in square hectares per year by clicking on a specific region. The size of '
+                       'the locations represents the proportion of burnt area in comparison to the other regions or '
+                       'counties.  For these two visualizations the Northern zone is represented by GREEN, '
+                       'the Central zone is represented by BEIGE, and the Southern zone is represented by BLUE. On '
+                       'the far right is a map depicting the number of fires per year for each region. Hover over '
+                       'each region for specific information about the number of fires. Adjust the slider (above) to '
+                       'select the year depicted in these three visualizations.', style={'padding': '1%', 'font-size': '90%'})
             ], className='text_box'),
 
             html.Div([
@@ -65,7 +73,7 @@ app.layout = html.Div([
             ], className='graph_box'),
 
             html.Div([
-                html.H4('TITLE'),
+                html.P('TITLE'),
                 dcc.RangeSlider(2001, 2018, 1, value=[2001, 2003],
                                 marks={i: '{}'.format(i) for i in range(2001, 2019)},
                                 tooltip={"placement": "bottom", "always_visible": True}, dots=True, pushable=2,
@@ -73,7 +81,13 @@ app.layout = html.Div([
             ], className='range_slider_box'),
 
             html.Div([
-                html.H6('Title')
+                html.P('Now that you know the dimension of the problem and where it is happening, it is important to '
+                        'understand what is causing it. For the figure on the left, hover over the image to see the '
+                        'total area burned by subcategory. Is Portugal reacting properly to the situation? The '
+                        'visualization on the right shows the burnt area and fire brigade expenditure over time. '
+                        'Spending tends to increase a small bit in the years after a large fire and then levels off. '
+                        'The ratio of spending to area burned is shown with the black line.  Adjust the range slider '
+                        '(above) to select the range of years depicted in both visualizations.',  style={'padding': '1%', 'font-size': '90%'})
             ], className='text_box'),
 
             html.Div([
@@ -104,6 +118,8 @@ def map_update(year):
     mapdata = map_df.merge(regions, left_on=map_df.index, right_on='region').set_index('region')
 
     fig = px.density_mapbox(mapdata, lat='lat', lon='lon', z=np.log(mapdata['total_ba']), radius=60,
+                            labels={'z': 'log(Burnt Area)'},
+                            hover_data={'region': mapdata.index},
                             center=dict(lat=39.557191, lon=-7.8536599), zoom=6,
                             opacity=1,
                             color_continuous_scale='solar_r',
